70d2d126ac6fcfd2fc9d6a67ca664b6fdba5a839
DucHuy-b <24022799@vnu.edu.vn>
2025-03-30 01:37:19 +0700

    Update Câu 4.cpp

--- BTGKT/II. Tự luận/Câu 4.cpp
+++ BTGKT/II. Tự luận/Câu 4.cpp
@@ -1,1 +1,22 @@
+int binarySearch(int arr[], int l, int r, int x)
+{
+  int m=(r+l)/2;
+  
+  if(l>r)
+    return -1;
+  
+  if(arr[m]==x)
+  {
+      return m;
+  }
+  else if(arr[m]>x)
+  {
+      return binarySearch(arr[], l, m-1, x);
+  }
+  else
+  {
+      return binarySearch(arr[], m+1, r, x);
+  }
 
+  return 0;
+}
